104c41b10583f41a1d33d58b0a1d7771d44628df
Rems <remi.vergnon@free.fr>
2017-03-14 16:02:05 +0000

    edits to match_env and new function to format eventDate in obis data

---
+++ R/format_obis_date.R
@@ -0,0 +1,25 @@
+#' Extract separate year, month and day information from obis data
+#'
+#' The function takes an OBIS occurrence data set and
+#' uses its eventDate columns to extract year, month
+#' and day information.
+#'
+#' \code{format_obis_date} formats the "eventDate" column
+#' in the data frame returned by robis::occurrence function.
+#' The eventDate entry is of the form yyyy-mm-dd hh:mm:ss,
+#' a format that needs splitting into year, month and day
+#' information to be useful. 
+#' 
+#' @param obis_data is a data.frame of OBIS occurrence data 
+#' obtained using robis::occurrence or merpWS::plot_obis
+#' 
+#' 
+format_obis_date <- function(obis_data){
+  inter <- strsplit(as.character(obis_data$eventDate), split = " ") %>%
+    sapply(., function(x) x[[1]]) %>%
+    strsplit(., split = "-") 
+  obis_data$year <- sapply(inter, function(x) x[1])
+  obis_data$month <- sapply(inter, function(x) x[2])
+  obis_data$day <- sapply(inter, function(x) x[3])
+  obis_data
+}
--- R/match_env.R
+++ R/match_env.R
@@ -1,27 +1,43 @@
-#' Match biotic and abiotic data in space
+#' Match biotic and abiotic data in space and time
 #'
-#' The function takes two sets of geolocated data (typically one biotic
-#' and one abiotic) and match their locations to combine the two.
-
+#' The function takes two sets of geolocated, temporally resolved
+#' data (typically one biotic and one abiotic) and match them.
+#'
 #' \code{match_env} returns the value of the environmental variable
-#' for the location in the abiotic set of coordinates that is closest to the 
-#' location for the biotic record. If the vector of values for 
+#' for the time and location in the abiotic set of coordinates that
+#' are closest to that of the biotic record. If the vector of values for 
 #' the abiotic variable is not provided, the function simply returns the
-#' index of the closest location in the vector of coordinates. 
+#' index of the closest location in the vector of coordinates. Temporal
+#' matching is done on a "year-month" basis. If there is no temporal
+#' match the function returns NA. Similarly if there is no matching data within
+#' a threshold distance (optional), the function returns NA. Geographical
+#' distance is calculating using the geosphere::distm() function using
+#' the haversine method (i.e. assuming a spherical earth, ignoring ellipsoidal effects). 
 #' 
 #' @param env_lon is a vector of longitudes for the abiotic variable
 #' @param env_lat is a vector of latitude for the abiotic variable
-#' @param bio_lon is the longitude of the biotic variable
-#' @param bio_lat is the latitude of the biotic variable
+#' @param env_time is a list containing vectors "year" and "month" 
+#' for the abiotic variables
+#' @param bio_lon is the longitude of the observed biotic record
+#' @param bio_lat is the latitude of the observed biotic record
+#' @param bio_time is a list containing the year and month at which the
+#' biotic record was observed
 #' @param max_dist is the maximum distance (in km) beyond which
 #' a environmental data point is considered too far to inform conditions
 #' at location of biological records
-#' @param env_variable is an optional vector containing the value of the abiotic variable
+#' @param env_variable is an optional vector containing abiotic variable values
 #' 
 #' 
-match_env <- function(env_lon, env_lat, bio_lon, bio_lat, max_dist = NA, env_variable = NULL){
-  alldist <- sapply(c(1:length(env_lon)), function(i) distm(c(env_lon[i], env_lat[i]), c(bio_lon, bio_lat)))
-  res <- ifelse(!is.null(env_variable), env_variable[which.min(alldist)], which.min(alldist))
-  if(!is.na(max_dist) & res > max_dist) return(NA)
-  else{return(res)}
+match_env <- function(env_lon, env_lat, env_time = c(year = NULL, month = NULL), bio_lon , bio_lat, bio_time = 
+                        c(year = NULL, month = NULL), max_dist = NA, env_variable = NULL){
+  idx <- which(bio_time[["year"]] == env_time[["year"]] & bio_time[["month"]] == env_time[["month"]])
+  res <- NA
+  if(length(idx) != 0){
+    env_lon <- env_lon[idx]
+    env_lat <- env_lat[idx]
+    alldist <- sapply(c(1:length(env_lon)), function(i) distm(c(env_lon[i], env_lat[i]), c(bio_lon, bio_lat)))
+    res <- ifelse(!is.null(env_variable), env_variable[idx[which.min(alldist)]], idx[which.min(alldist)])
+    if(!is.na(max_dist) & res > max_dist) res <- NA
+    }
+  res
 }
